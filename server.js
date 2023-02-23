2b82899a3b3703dfd15c98d6ff938bf1ebf926cb
p0ptartz <ericwm420@gmail.com>
2023-02-22 21:08:05 -0500

    set up express server

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express")
+
+const app = express()
+
+const PORT = 3001
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`)
+})
