bf5ba3e46395042fee52ddf0265e915e85874b0a
p0ptartz <ericwm420@gmail.com>
2023-02-24 23:47:11 -0500

    wrote Thought model

---
+++ models/Thought.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+
+const thoughtSchema = new mongoose.Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            // Use a getter method to format the timestamp on query ????????
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [reactionSchema]
+    }
+)
+
+const reactionSchema = new mongoose.Schema(
+    {
+        reactionId: {
+            type: mongoose.Types.ObjectId,
+            default: mongoose.Types.ObjectId
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+            // Use a getter method to format the timestamp on query ??????
+        }
+    }
+)
+
+thoughtSchema.virtual("reactionCount").get(function () {
+    return this.reactions.length;
+})
+
+const Thought = mongoose.model("Thought", thoughtSchema)
+
+module.exports = Thought
