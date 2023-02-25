1602d09eef30906ea0cea290c059dde031e7bc8b
p0ptartz <ericwm420@gmail.com>
2023-02-25 00:03:15 -0500

    finished User model... i hope...

--- models/User.js
+++ models/User.js
@@ -13,6 +13,15 @@ const userSchema = new mongoose.Schema(
             required: true,
             unique: true,
             match: [/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/, 'Enter a valid email']
-        }
+        },
+        thoughts: [{ type: mongoose.Schema.Types.ObjectId, ref: "Thought" }],
+        friends: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }]
     }
 )
+
+userSchema.virtual("friendCount").get(function () {
+    return this.friends.length
+})
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
