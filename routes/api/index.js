395e695ffc593257541038b97366d34335b6f125
p0ptartz <ericwm420@gmail.com>
2023-02-28 21:47:24 -0500

    finished api/index.js files

---
+++ routes/api/index.js
@@ -0,0 +1,9 @@
+const router = require("express").Router()
+
+const thoughtsRoutes = require("./thoughts")
+const userRoutes = require("./users")
+
+router.use("/thoughts", thoughtsRoutes);
+router.use("/users", userRoutes);
+
+module.exports = router;
