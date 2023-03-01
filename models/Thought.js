82cf897eacd379ca695cd7cbd0acaaa76150f807
p0ptartz <ericwm420@gmail.com>
2023-02-28 21:59:49 -0500

    had to change order

--- models/Thought.js
+++ models/Thought.js
@@ -1,26 +1,5 @@
 const mongoose = require('mongoose');
 
-const thoughtSchema = new mongoose.Schema(
-    {
-        thoughtText: {
-            type: String,
-            required: true,
-            minlength: 1,
-            maxlength: 280
-        },
-        createdAt: {
-            type: Date,
-            default: Date.now,
-            // Use a getter method to format the timestamp on query ????????
-        },
-        username: {
-            type: String,
-            required: true
-        },
-        reactions: [reactionSchema]
-    }
-)
-
 const reactionSchema = new mongoose.Schema(
     {
         reactionId: {
@@ -48,6 +27,27 @@ const reactionSchema = new mongoose.Schema(
     },
     id: true
 })
+
+const thoughtSchema = new mongoose.Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            // Use a getter method to format the timestamp on query ????????
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [reactionSchema]
+    }
+)
 
 thoughtSchema.virtual("reactionCount").get(function () {
     return this.reactions.length;
